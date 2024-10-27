bc0bf796b5e7a9c4dff720540bafd12416832d44
Rael CALITRO <rael06@hotmail.fr>
2024-10-27 22:46:47 +0100

    add env

--- Src/Controllers/BaseController.cs
+++ Src/Controllers/BaseController.cs
@@ -1,16 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace MovieAppApi.Src.Controllers
-{
-  [ApiController]
-  [Route("api/[controller]")]
-  public class BaseController<T> : Controller
-  {
-    protected readonly ILogger<T> _logger;
+namespace MovieAppApi.Src.Controllers;
 
-    public BaseController(ILogger<T> logger)
-    {
-      _logger = logger;
-    }
+[ApiController]
+[Route("api/[controller]")]
+public class BaseController<T> : Controller
+{
+  protected readonly ILogger<T> _logger;
+
+  public BaseController(ILogger<T> logger)
+  {
+    _logger = logger;
   }
 }
--- Src/Controllers/HealthController.cs
+++ Src/Controllers/HealthController.cs
@@ -1,17 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
+using MovieAppApi.Src.Core.Services.Environment;
 
-namespace MovieAppApi.Src.Controllers
+namespace MovieAppApi.Src.Controllers;
+
+public class HealthController : BaseController<HealthController>
 {
-  public class HealthController : BaseController<HealthController>
+  public HealthController(ILogger<HealthController> logger, IEnvService _envService) : base(logger)
   {
-    public HealthController(ILogger<HealthController> logger) : base(logger)
-    {
-    }
+    var tmdbApiKey = _envService.Vars.TmdbApiKey;
+    Console.WriteLine(tmdbApiKey);
+  }
 
-    [HttpGet]
-    public IActionResult Get()
-    {
-      return Ok("Ok");
-    }
+  [HttpGet]
+  public IActionResult Get()
+  {
+    return Ok("Ok");
   }
 }
